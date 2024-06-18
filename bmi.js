6f219888f641770bbffba4c04d71576c91c1f97b
cmcreators237 <cmcreators237@gmail.com>
2024-06-18 13:32:41 +0100

    added some codes

--- bmi.js
+++ bmi.js
@@ -1,24 +1,24 @@
 let weight = 80.8;
 let height = 1.8;
-let bmi = weight / (height * height);
+  let bmi = weight / (height ** 2);
 console.log(bmi);
 let classify;
 
 if (bmi < 16) {
-  classify = `Severe Thinness and  Bmi is : ${bmi}`;
+  classify = `Severe Thinness `;
 } else if (bmi >= 16 && bmi < 17) {
-  classify = `Moderate Thinness and class is : ${bmi}`;
+  classify = `Moderate Thinness `;
 } else if (bmi >= 17 && bmi < 18.5) {
-  classify = `Mild Thinness and class is : ${bmi}`;
+  classify = `Mild Thinness `;
 } else if (bmi >= 18.5 && bmi < 25) {
-  classify = `Normal and class is : ${bmi}`;
+  classify = `Normal `;
 } else if (bmi >= 25 && bmi < 30) {
-  classify = `Overweight and class is : ${bmi}`;
+  classify = `Overweight `;
 } else if (bmi >= 30 && bmi < 35) {
-  classify = `Obese Class I and class is : ${bmi}`;
+  classify = `Obese Class I `;
 } else if (bmi >= 35 && bmi < 40) {
-  classify = `Obese Class II and class is : ${bmi}`;
+  classify = `Obese Class II `;
 } else if (bmi >= 40) {
-  classify = `Obese Class IIIand class is : ${bmi}`;
+  classify = `Obese Class III`;
 }
-console.log(classify);
+console.log(`${classify} and bmi is ${bmi}`);
---
+++ temp.js
@@ -0,0 +1,19 @@
+/** @format */
+
+//converting fahrenheit into  celsius (F-32)/1.8  =CEL
+//converting  celsius into  fahrenheit  (C X 32)+1.8  =FAH.
+let temperature;
+
+const unit = prompt(
+	"Enter your Unit to get  currenct temperate F for Fahrenheit  C for Celsius"
+);
+
+if (unit === "C") {
+	let result = Number(prompt("Enter your location Weather degrees to convert into Fahrenheit"));
+	temperature = ( result * 1.8) + 32;
+	alert(`${result}°C is ${temperature}°F`);
+}else if(unit === 'F'){
+    let  result = Number(prompt("Enter your location Weather degrees to convert into Celsius"));
+	temperature = (result - 32 )/1.8;
+	alert(`${result}°F is ${temperature}°C`);
+}
