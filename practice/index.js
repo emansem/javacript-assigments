d1aacbf9d6b68de348cdeeb81d0425abc024dbe6
cmcreators237 <cmcreators237@gmail.com>
2024-06-20 16:19:30 +0100

    added some codes

--- practice/index.js
+++ practice/index.js
@@ -1,26 +1,26 @@
 /** @format */
 
 const shoppingList = [
-	{
-		name: "Shoes",
-		quantity: 1,
-		price: 20,
-	},
-	{
-		name: "Shoes",
-		quantity: 1,
-		price: 20,
-	},
+  {
+    name: "Shoes",
+    quantity: 1,
+    price: 20,
+  },
+  {
+    name: "Shoes",
+    quantity: 1,
+    price: 20,
+  },
 ];
 
 function addNewProduct(name, quantity, price) {
-	const newProduct = {
-		name: name,
-		quantity: quantity,
-		price: price,
-	};
+  const newProduct = {
+    name: name,
+    quantity: quantity,
+    price: price,
+  };
 
-	shoppingList.push(newProduct);
+  shoppingList.push(newProduct);
 }
 
 addNewProduct("car", 4, 2000);
@@ -33,42 +33,42 @@ console.log("updated shopping list", shoppingList);
 //Task 4: Add to Cart
 // Write a Method: Write a method to add items to a cart. This cart should be a separate array where selected items for purchase are stored.
 const cart = [
-	{
-		prodId: "bag",
-		prodQty: 3,
-		prodPrice: 3000,
-	},
-	{
-		prodId: "shoes",
-		prodQty: 1,
-		prodPrice: 1500,
-	},
-	{
-		prodId: "shoes",
-		prodQty: 1,
-		prodPrice: 1500,
-	},
+  {
+    prodId: "bag",
+    prodQty: 3,
+    prodPrice: 3000,
+  },
+  {
+    prodId: "shoes",
+    prodQty: 1,
+    prodPrice: 1500,
+  },
+  {
+    prodId: "shoes",
+    prodQty: 1,
+    prodPrice: 1500,
+  },
 ];
 
 function addtoCart(prodId, quantity, price) {
-	let matchProduct = cart.find((item) => item.prodId === prodId);
+  let matchProduct = cart.find((item) => item.prodId === prodId);
 
-	if (matchProduct) {
-		matchProduct.prodQty += quantity;
-		matchProduct.prodPrice += price; // Update quantity if product already exists
-	} else {
-		// Add new product to cart if not already present
-		const product = shoppingList.find((item) => item.name === prodId);
-		if (product) {
-			cart.push({
-				prodId: prodId,
-				prodQty: quantity,
-				prodPrice: product.price * quantity,
-			});
-		}
-	}
+  if (matchProduct) {
+    matchProduct.prodQty += quantity;
+    matchProduct.prodPrice += price; // Update quantity if product already exists
+  } else {
+    // Add new product to cart if not already present
+    const product = shoppingList.find((item) => item.name === prodId);
+    if (product) {
+      cart.push({
+        prodId: prodId,
+        prodQty: quantity,
+        prodPrice: product.price * quantity,
+      });
+    }
+  }
 
-	console.log("Updated Cart:", cart);
+  console.log("Updated Cart:", cart);
 }
 
 addtoCart("shoes", 3, 900);
@@ -76,6 +76,6 @@ addtoCart("shoes", 3, 900);
 
 let totalQty = 0;
 cart.forEach((item) => {
-	totalQty += item.prodPrice;
+  totalQty += item.prodPrice;
 });
 console.log(totalQty);
